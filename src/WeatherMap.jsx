be761ee3353b9877dd9ebd153c931f890443faa3
Odoje Oladayo Jacob <dayodoje@gmail.com>
2026-06-25 16:48:06 +0100

    Update

--- src/WeatherMap.jsx
+++ src/WeatherMap.jsx
@@ -1,9 +1,35 @@
-import React from "react";
-import { FaCloudRain, FaSearch } from "react-icons/fa";
+import React, { useEffect, useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import nigeriaCities from "../src/data/nigeriaCities";
+
+import { WiHumidity } from "react-icons/wi";
+import { FaCloudRain, FaSearch, FaWind } from "react-icons/fa";
+import { MdOutlineVisibility } from "react-icons/md";
+import { SiEspressif } from "react-icons/si";
 
 const WeatherMap = () => {
+  const [weather, setWeather] = useState(null);
+  const [selectedStates, setSelectedStates] = useState(null);
+  const apiKey = import.meta.env.VITE_OPENWEATHER_API_KEY;
+
+  const fetchWeather = async (city) => {
+    setSelectedStates(city);
+    try {
+      const res = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${city.coordinates[0]}&lon=${city.coordinates[1]}&appid=${apiKey}&units=metric`,
+      );
+      const data = await res.json();
+      console.log("Weather data:", data);
+      setWeather(data);
+    } catch (err) {
+      console.error("There was an error fatching Weaher Data:", err);
+    }
+  };
+  useEffect(() => {
+    fetchWeather(nigeriaCities[2]);
+  }, []);
+
   return (
     <div className="">
       <header className="bg-[#1a3a5e] p-8">
@@ -37,25 +63,108 @@ const WeatherMap = () => {
           </div>
         </div>
       </header>
-      <div className="w-[70%]">
-        <MapContainer
-          center={[51.505, -0.09]}
-          zoom={13}
-          scrollWheelZoom={false}
-          className="w-full h-screen"
-        >
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <Marker position={[51.505, -0.09]}>
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-          </Marker>
-        </MapContainer>
+      <div className="flex">
+        <div className="w-[70%]">
+          <MapContainer
+            center={[9.082, 8.6753]}
+            zoom={6}
+            scrollWheelZoom={false}
+            className="w-full h-screen"
+          >
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            {nigeriaCities.map((city) => (
+              <Marker
+                position={city.coordinates}
+                eventHandlers={{
+                  click: () => {
+                    fetchWeather(city);
+                  },
+                }}
+                key={city.name}
+              >
+                <Popup>{city.name}</Popup>
+              </Marker>
+            ))}
+          </MapContainer>
+        </div>
+        <div className="w-[30%] ">
+          {weather ? (
+            <div className="p-4">
+              <h1 className="text-xl text-gray-200 font-semibold">
+                {selectedStates.name}
+              </h1>
+              <small className="text-gray-400">{selectedStates.zone}</small>
+              <div className="border-t border-gray-600 mt-2 flex items-center justify-between">
+                <div className="mt-3">
+                  <h1 className="text-5xl font-bold text-gray-200">
+                    {Math.round(weather.main.temp)}°C
+                  </h1>
+
+                  <small className="text-gray-400 capitalize">
+                    {weather.weather[0].description}
+                  </small>
+
+                  <p className="text-gray-500 text-xs mt-1">
+                    Feels like {Math.round(weather.main.feels_like)}°C
+                  </p>
+                </div>
+                <img
+                  src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+                  alt={weather.weather[0].description}
+                  className="w-16 h-16"
+                />
+              </div>
+              <div className="border-t border-gray-600 mt-2 grid grid-cols-2">
+                <div className="mt-3">
+                  <div className="flex items-center gap-1">
+                    <WiHumidity />
+                    <small className="text-gray-400">Humidity</small>
+                  </div>
+                  <h3 className="text-xl text-gray-200 font-semibold">
+                    {weather.main.humidity} %
+                  </h3>
+                </div>
+                <div className="mt-3">
+                  <div className="flex items-center gap-1">
+                    <FaWind />
+                    <small className="text-gray-400">Wind</small>
+                  </div>
+                  <h3 className="text-xl text-gray-200 font-semibold">
+                    {weather.wind.speed} km/h
+                  </h3>
+                </div>
+                <div className="mt-3">
+                  <div className="flex items-center gap-1">
+                    <MdOutlineVisibility />
+                    <small className="text-gray-400">Visibility</small>
+                  </div>
+                  <h3 className="text-xl text-gray-200 font-semibold">
+                    {weather.visibility / 1000} km
+                  </h3>
+                </div>
+                <div className="mt-3">
+                  <div className="flex items-center gap-1">
+                    <SiEspressif />
+                    <small className="text-gray-400">Pressure</small>
+                  </div>
+                  <h3 className="text-xl text-gray-200 font-semibold">
+                    {weather.main.pressure} hPa
+                  </h3>
+                </div>
+              </div>
+            </div>
+          ) : (
+            <div>
+              <p className="text-gray-400 text-sm">
+                Click any city marker to see its weather
+              </p>
+            </div>
+          )}
+        </div>
       </div>
-      <div className="w-[30%]"></div>
     </div>
   );
 };
